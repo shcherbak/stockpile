13a8c44854d55d548e4713c1178a1fdc8f155621
Yurii Shcherbak <shcherbak@localhost.localdomain>
2017-05-11 18:05:11 +0300

    adapt

--- api/wms-api/dal.py
+++ api/wms-api/dal.py
@@ -1,10 +1,58 @@
 #!/usr/bin/env python
-
 import psycopg2
 import psycopg2.extensions
 import psycopg2.extras
 from config import config
 
+
+class CommonDocumentHead_adapter(object):
+
+    def __init__(self, document_id, gid, display_name, document_date, facility_code, curr_fsmt, doctype):
+        self._document_id = document_id
+        self._gid = gid
+        self._display_name = display_name
+        self._document_date = document_date
+        self._facility_code = facility_code
+        self._curr_fsmt = curr_fsmt
+        self._doctype = doctype
+
+    def __conform__(self, proto):
+        if proto is psycopg2.extensions.ISQLQuote:
+            return self
+
+    def getquoted(self):
+        return ("'%s'::common.document_head" % self._document_id).encode('utf8')
+
+    def __str__(self):
+        return "'%s'::common.document_head" % self._document_id
+
+
+
+def register_document_head(oids=None, conn_or_curs=None):
+    import uuid
+
+    if not oids:
+        oid1 = 2950
+        oid2 = 2951
+    elif isinstance(oids, (list, tuple)):
+        oid1, oid2 = oids
+    else:
+        oid1 = oids
+        oid2 = 2951
+
+    DOCUMENT_HEAD = psycopg2.extras.new_type((oid1, ), "DOCUMENT_HEAD",
+            lambda data, cursor: data and uuid.UUID(data) or None)
+    DOCUMENT_HEAD_ARRAY = psycopg2.extras.new_array_type((oid2,), "DOCUMENT_HEAD[]", DOCUMENT_HEAD)
+
+    psycopg2.extras.register_type(DOCUMENT_HEAD, conn_or_curs)
+    psycopg2.extras.register_type(DOCUMENT_HEAD_ARRAY, conn_or_curs)
+    psycopg2.extras.register_adapter(DOCUMENT_HEAD, CommonDocumentHead_adapter)
+
+    return DOCUMENT_HEAD
+
+
+def castDocumentHead(s, curs):
+    if s is not None: return "(DocumentHead) " + s
 
 def get_demand_body(__document_id):
     """ get parts provided by a vendor specified by the vendor_id """
@@ -18,6 +66,7 @@ def get_demand_body(__document_id):
         cur.callproc('demand.get_body', (__document_id, ))
         #body = cur.fetchall()
         row = cur.fetchone()
+        print(type(row))
 
         while row is not None:
             print(row)
@@ -42,9 +91,11 @@ def get_demand_head(__document_id):
         conn = psycopg2.connect(**params)
         cur = conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
         #cur = conn.cursor()
+        psycopg2.extras.register_composite('common.document_head', cur)
         cur.callproc('demand.get_head', (__document_id, ))
         #body = cur.fetchall()
         row = cur.fetchone()
+        print(type(row))
 
         while row is not None:
             print(row)
@@ -64,7 +115,5 @@ def get_demand(f):
     return f;
 
 if __name__ == '__main__':
-    get_demand_body(1)
-    get_demand_body(2)
     get_demand_body(81)
     get_demand_head(81)
