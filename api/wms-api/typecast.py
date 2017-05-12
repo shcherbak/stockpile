8bdc6ad06ca42426c29a7ab0f881f7ff64188d4c
Yurii Shcherbak <shcherbak@localhost.localdomain>
2017-05-12 17:32:56 +0300

    python custom type mapping resolved

--- api/wms-api/typecast.py
+++ api/wms-api/typecast.py
@@ -3,104 +3,49 @@
 import re
 import psycopg2
 import psycopg2.extensions
+import psycopg2.extras
 from config import config
-
-
-#CREATE TYPE public.c_txt AS
-#   (a character varying,
-#    b character varying);
-#ALTER TYPE public.c_txt
-#  OWNER TO postgres;
-
-class CTxt(object):
-
-    def __init__(self, s=None, curs=None):
-
-        self.a = ''
-        self.b = ''
-
-        if s:
-            self.from_string(s)
-
-
-    def __conform__(self, proto):
-        if proto == psycopg2.extensions.ISQLQuote:
-            return self
-
-    def from_string(self, s):
-        if s is None:
-            return None
-        m = re.match(r"\(([^)]+),([^)]+)\)", s)
-        print (m)
-        print (m.group(1))
-        print (m.group(2))
-        if m:
-            return (m.group(1), m.group(2))
-        else:
-            raise psycopg2.extensions.InterfaceError("bad point representation: %r" % s)
-
-    def getquoted(self):
-        """Format self as a string usable by the db to represent a box."""
-        s = "'(%s,%s)'" % (self.a, self.b)
-        return s
-
-    def show(self):
-        """Format a description of the box."""
-        s = "A: %s\tB: %s" % (self.a, self.b)
-        return s
-
 
 params = config()
 conn = psycopg2.connect(**params)
 curs = conn.cursor()
-curs.execute("select ('a','b')::public.c_txt as foo")
-ctxt_oid = curs.description[0][1]
-print (("Oid for the box datatype is"), ctxt_oid)
-C_TXT = psycopg2.extensions.new_type((ctxt_oid,), "C_TXT", CTxt)
-psycopg2.extensions.register_type(C_TXT)
+psycopg2.extras.register_composite('common.document_body', curs, True)
+psycopg2.extras.register_composite('common.stoktake_body', curs, True)
 
-curs.execute("select ('a','b')::public.c_txt as foo1")
-curs.execute("select ('c','d')::public.c_txt as foo2")
-boxes = curs.fetchall()
-print ("Found %d boxes with at least a point inside:" % len(boxes))
-for box in boxes:
-    print (" ", box[0].show())
+r = ('hello', 1, 'world')
+curs.execute("select %s::common.document_body;", (r,))
 
 
+print ("------")
+box0 = curs.fetchone()
+print (type(box0[0]))
+print ("------")
+print (box0[0])
+print (type(box0[0][1]))
 
 
+r = ('hello', 1, 'world', 100)
+curs.execute("select %s::common.stoktake_body;", (r,))
+print ("------")
+box0 = curs.fetchone()
+print (type(box0[0]))
+print ("------")
+print (box0[0])
+print (type(box0[0][1]))
 
-def print_type_oid():
-    conn = None
-    try:
-        params = config()
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        cur.execute("SELECT NULL::common.quantity;")
-        print(cur.description)
-        cur.execute("SELECT NULL::common.document_body;")
-        print(cur.description)
-        cur.execute("SELECT NULL::common.document_body[];")
-        print(cur.description)
-        cur.execute("SELECT NULL::common.document_head;")
-        print(cur.description)
-        cur.execute("SELECT NULL::common.document_head[];")
-        print(cur.description)
-        cur.execute("SELECT NULL::common.outbound_head;")
-        print(cur.description)
-        cur.execute("SELECT NULL::common.outbound_head[];")
-        print(cur.description)
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+
+curs.callproc("demand.get_body", (81,))
+print ("------")
+box0 = curs.fetchone()
+print (type(box0))
+print (type(box0[0]))
+print (type(box0[0][0]))
+print (box0[0][0])
+print (box0[0][1])
+print ("------")
+#print (box0[0])
+#print (type(box0[0][1]))
 
 
 if __name__ == '__main__':
-    #print_type_oid()
-    c_txt = CTxt()
-    print(c_txt.from_string("(abc,dfg)"))
-    print ("A", c_txt.a)
     pass
